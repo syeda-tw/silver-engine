a09e2fd4650ac57629e434a06f09ff8385ce2a2e
s-zehra <zehrataqwi@gmail.com>
2025-08-10 21:39:17 +0300

    wip: added starting animation logic for loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Input from "@/app/components/input";
 import Loader from "@/app/components/loader";
 import Button from "./components/button";
@@ -5,6 +8,50 @@ import Image from "next/image";
 import ChevronLeftIcon from "@/public/icons/chevronLeft.svg";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isLoaderShown, setIsLoaderShown] = useState(false);
+  const [isInputChangeDisabled, setIsInputChangeDisabled] = useState(false);
+  const []
+
+
+  const startAnimation = () => {
+    setTimeout(() => {
+      setIsLoaderShown(false);
+    }, 1000);
+  }
+
+  // Function that runs 1 seconds after email changes
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      // Email validation regex
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const isValid = emailRegex.test(email);
+      if(isValid) {
+          setIsEmailValid(isValid);
+          setIsLoaderShown(true);
+          setIsInputChangeDisabled(true);
+          startAnimation();
+      } else {
+        setIsEmailValid(false);
+        setIsLoaderShown(false);
+        setIsInputChangeDisabled(false);
+      }
+    }, 1000);
+
+    // Cleanup function to clear timeout if email changes again
+    return () => clearTimeout(timeoutId);
+  }, [email]);
+
+  useEffect(() => {
+    if(isEmailValid) {
+      setIsLoaderShown(true);
+    } else {
+      setIsLoaderShown(false);
+    }
+  }, [isEmailValid]);
+
+
   return (
     <div className="flex h-full w-full items-center justify-center px-2">
       <div className="flex flex-col w-[660px] max-w-full">
@@ -20,15 +67,18 @@ export default function Home() {
         <Input
           type="text"
           placeholder="Enter your email"
-          rightIcon={Loader}
+          rightIcon={isLoaderShown ? Loader : undefined}
           className="mb-4"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={isInputChangeDisabled}
         />
         <div className="flex flex-row justify-between mt-2">
           <Button variant="ghost">
             <Image src={ChevronLeftIcon} alt="Back" className="mr-2 w-2 h-2 inline" />
             Back
           </Button>
-          <Button disabled>Next</Button>
+          <Button disabled={!isEmailValid}>Next</Button>
         </div>
       </div>
     </div>
